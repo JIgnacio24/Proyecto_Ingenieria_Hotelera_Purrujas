f158330abc72aacbb5fff6d9175aa74c0a81d421
Celeste2002 <macesaul18@outlook.es>
2026-04-04 19:35:33 -0600

    Facilities como componente
    Ahora facilities es un componente aparte para hacerlo más ordenado por si hace falta luego agregar o quitar algo

--- Frontend/Frontend-Ingenieria-Purrujas-Cliente/src/app/modules/about-us/about-us.ts
+++ Frontend/Frontend-Ingenieria-Purrujas-Cliente/src/app/modules/about-us/about-us.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FacilitiesComponent } from '../facilities/facilities.component';
  
 @Component({
   selector: 'app-about-us',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, FacilitiesComponent],
   templateUrl: './about-us.html',
   styleUrl: './about-us.css'
 })
---
+++ Frontend/Frontend-Ingenieria-Purrujas-Cliente/src/app/modules/facilities/facilities.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-facilities',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './facilities.component.html',
+  styleUrl: './facilities.component.css'
+})
+export class FacilitiesComponent {}
